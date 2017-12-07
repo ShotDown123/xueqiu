ec02d114d80b9051bb51c4fdf4522636f7b0d0f8
ShotDown123 <34236438+ShotDown123@users.noreply.github.com>
2017-12-07 23:06:55 +0800

    Update snowball.py
    获取收藏文章，并按照表格的形式记录文章名地址等

--- snowball.py
+++ snowball.py
@@ -4,6 +4,11 @@ __author__ = 'Rocky'
 import requests,cookielib,re,json,time
 from toolkit import Toolkit
 from lxml import etree
+import codecs
+import sys
+
+
+
 url='https://xueqiu.com/snowman/login'
 session = requests.session()
 
@@ -35,8 +40,14 @@ maxPage=p.findall(fav_content)[0]
 print maxPage
 print type(maxPage)
 maxPage=int(maxPage)
+index = 0
+host='https://xueqiu.com'
+cp_host = 'https://xueqiu.com/S/'
+save_data = 'index  | name | code | at'
+save_data = save_data + '\r\n' + '----- | ----- | ----- | -----'
+
 print type(maxPage)
-for i in range(1,maxPage+1):
+for i in range(1,maxPage-2):
     fav='https://xueqiu.com/favs?page=%d' %i
     collection=session.get(fav,headers=headers)
     fav_content= collection.text
@@ -49,23 +60,33 @@ for i in range(1,maxPage+1):
     #print new_result
     data=json.loads(new_result)
     use_data= data['list']
-    host='https://xueqiu.com'
     for i in use_data:
-        url=host+ i['target']
-        print url
-        txt_content=session.get(url,headers=headers).text
-        #print txt_content.text
-
-        tree=etree.HTML(txt_content)
-        title=tree.xpath('//title/text()')[0]
-
-        filename = re.sub('[\/:*?"<>|]', '-', title)
-        print filename
-
-        content=tree.xpath('//div[@class="detail"]')
-        Toolkit.save2filecn(filename,"Link: %s\n\n" %url)
-        for i in content:
-            Toolkit.save2filecn(filename, i.xpath('string(.)'))
-        #print content
-        #Toolkit.save2file(filename,)
-        time.sleep(10)
+        index = index + 1
+        at_addr = host + i['target']
+        try:
+            at_content = session.get(at_addr,headers=headers).text.encode("UTF-8")
+        except:
+            time.sleep(1)
+            try:
+                at_content = session.get(at_addr,headers=headers).text.encode("UTF-8")
+            except:
+                print at_addr
+        at_name = re.findall('<h1 class="article__bd__title">(.*)</h1><div class="article__bd__detail">',at_content)
+        at_name[0] = at_name[0].replace('|','--')
+        cp_content = re.findall(r'\>\$(.*?)\$\<', at_content)
+        cp_code = ['cd_none']
+        cp_name = ['nm_none']
+        if len(cp_content) > 0:
+            cp_code = re.findall(r'\((.*)\)', cp_content[0])
+            cp_name = re.findall(r'(.*)\(', cp_content[0])
+        cp_addr = cp_host + cp_code[0]
+  
+        'index  | name | code | at'
+        save_data = save_data + '\r\n' + str(index).zfill(5) + ' | '+ cp_name[0]   + ' | [' + cp_code[0] + '](' + cp_addr.encode("UTF-8") + ')' + ' | [' + at_name[0] + '](' + at_addr.encode("UTF-8") + ')'
+        #print save_data
+        #save_data = save_data + '\r\n' + str(index).zfill(5) + ' | [' + at_name[0] + '](' + at_addr.encode("UTF-8") + ')'
+        time.sleep(1)
+#print save_data
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+Toolkit.save2filecn('data', save_data)
